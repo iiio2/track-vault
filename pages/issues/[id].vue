82289f804ae8c2cb95e69cf445fc928e26ed6064
Mr. Robot <gomexxxx71@gmail.com>
2023-12-04 18:46:22 +0600

    chore: get issue

--- pages/issues/[id].vue
+++ pages/issues/[id].vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute()
+interface Issue {
+  issue: {
+    title: string
+    description: string
+    status: string
+  }
+}
+const { data } = await useFetch<Issue>(`/api/issues/${route.params.id}`)
+</script>
 
 <template>
-  <div>each issue {{ $route.params.id }}</div>
+  <div>
+    <h2 class="text-3xl">{{ data?.issue.title }}</h2>
+    <StatusBadge :status="data?.issue.status" class="my-2" />
+    <p>{{ data?.issue.description }}</p>
+  </div>
 </template>
