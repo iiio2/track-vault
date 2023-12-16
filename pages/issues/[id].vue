bc517919c4949c3f5ce5f0e04ff24362e84ae18a
Mr. Robot <gomexxxx71@gmail.com>
2023-12-16 22:32:42 +0600

    chore: add date in each issue

--- pages/issues/[id].vue
+++ pages/issues/[id].vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import moment from 'moment'
 const route = useRoute()
 interface Issue {
   issue: {
     title: string
     description: string
     status: string
+    createdAt: string
   }
 }
 const { data } = await useFetch<Issue>(`/api/issues/${route.params.id}`)
@@ -15,5 +17,6 @@ const { data } = await useFetch<Issue>(`/api/issues/${route.params.id}`)
     <h2 class="text-3xl">{{ data?.issue.title }}</h2>
     <StatusBadge :status="data?.issue.status" class="my-2" />
     <p>{{ data?.issue.description }}</p>
+    <p>{{ moment(data?.issue.createdAt).format('DD MMM, YYYY') }}</p>
   </div>
 </template>
