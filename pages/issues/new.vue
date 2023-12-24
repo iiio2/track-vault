2e6fe6a99c840cf25adab8d23f807dd8d93d7f20
Mr. Robot <gomexxxx71@gmail.com>
2023-12-25 00:26:36 +0600

    chore: add toast notification

--- pages/issues/new.vue
+++ pages/issues/new.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { z } from 'zod'
+import { Toaster, toast } from '@steveyuowo/vue-hot-toast'
 import type { FormSubmitEvent } from '#ui/types'
 import { issueFormSchema } from '~/prisma/IssueFormSchema'
+import '@steveyuowo/vue-hot-toast/vue-hot-toast.css'
 
 type schema = z.output<typeof issueFormSchema>
 
@@ -11,18 +13,26 @@ const issue = ref({
 })
 
 const addIssue = async ({ data }: FormSubmitEvent<schema>) => {
-  await useFetch('/api/issues/issues', {
-    method: 'post',
-    body: {
-      title: data.title,
-      description: data.description,
-    },
-  })
+  try {
+    const { data: newIssue } = await useFetch('/api/issues/issues', {
+      method: 'post',
+      body: {
+        title: data.title,
+        description: data.description,
+      },
+    })
+    if (newIssue.value) {
+      toast.success('Issue added')
+    }
+  } catch (ex) {
+    toast.error('Something error occured')
+  }
 }
 </script>
 
 <template>
   <div class="w-full">
+    <Toaster />
     <UForm
       class="px-8 pt-6 pb-8 mb-4"
       :schema="issueFormSchema"
