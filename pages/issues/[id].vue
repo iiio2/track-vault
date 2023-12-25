c7fe4fe1c7754816ecf489729b2828ef76acabc0
Mr. Robot <gomexxxx71@gmail.com>
2023-12-25 23:14:46 +0600

    chore: styling

--- pages/issues/[id].vue
+++ pages/issues/[id].vue
@@ -13,7 +13,21 @@ const { data } = await useFetch<Issue>(`/api/issues/${route.params.id}`)
 
 <template>
   <div>
-    <h2 class="text-3xl">{{ data?.issue.title }}</h2>
+    <div class="flex justify-between">
+      <h2 class="text-3xl">{{ data?.issue.title }}</h2>
+      <div class="flex gap-5">
+        <button
+          class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm"
+        >
+          Edit Issue
+        </button>
+        <button
+          class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm"
+        >
+          Delete Issue
+        </button>
+      </div>
+    </div>
     <StatusBadge :status="data?.issue.status" class="my-2" />
     <p>{{ data?.issue.description }}</p>
     <p>{{ $time(data?.issue.createdAt!) }}</p>
