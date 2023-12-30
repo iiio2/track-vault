6b3774c829f2c2f50c3b1e6b6d6deaf922bd271c
Mr. Robot <gomexxxx71@gmail.com>
2023-12-30 10:28:54 +0600

    chore: add navbar on error page

--- error.vue
+++ error.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="grid place-items-center h-screen">
+  <Navbar />
+  <div class="grid place-items-center h-96">
     <h2 class="text-3xl">Error occured!</h2>
   </div>
 </template>
