9c2f0855db2aee8e29da98b92b6230a94c7f22da
Mr. Robot <gomexxxx71@gmail.com>
2024-05-04 22:39:39 +0600

    chore(api): implement concurrent operations

--- server/api/issues/[issueId].delete.ts
+++ server/api/issues/[issueId].delete.ts
@@ -2,13 +2,17 @@ import prisma from '~/prisma/client'
 
 export default defineEventHandler(async (event) => {
   const issueId = Number(getRouterParam(event, 'issueId'))
-  await useStorage('redis').clear()
-  await prisma.issue.delete({
-    where: {
-      id: issueId,
-    },
-  })
+
+  await Promise.all([
+    await useStorage('redis').clear(),
+    await prisma.issue.delete({
+      where: {
+        id: issueId,
+      },
+    }),
+  ])
+
   return {
-    message: 'issue deleted successfully',
+    message: 'Issue has been deleted successfully',
   }
 })
--- server/api/issues/[issueId].put.ts
+++ server/api/issues/[issueId].put.ts
@@ -3,16 +3,19 @@ import prisma from '~/prisma/client'
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   const id = Number(getRouterParam(event, 'issueId'))
-  await useStorage('redis').clear()
-  const issue = await prisma.issue.update({
-    where: {
-      id,
-    },
-    data: {
-      title: body.title,
-      description: body.description,
-      assignedUserId: body.assignedUserId ? body.assignedUserId : null,
-    },
-  })
-  return issue
+
+  const updatedIssue = await Promise.all([
+    await useStorage('redis').clear(),
+    await prisma.issue.update({
+      where: {
+        id,
+      },
+      data: {
+        title: body.title,
+        description: body.description,
+        assignedUserId: body.assignedUserId ? body.assignedUserId : null,
+      },
+    }),
+  ])
+  return updatedIssue
 })
